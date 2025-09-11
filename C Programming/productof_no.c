c1a4300c027119b2303f9f05fbd43be065eeedb4
vamsi <vamsikrishnapallam@gmail.com>
2025-09-11 14:10:13 +0530

    changed variable

--- C Programming/productof_no.c
+++ C Programming/productof_no.c
@@ -16,17 +16,17 @@ step11: stop
 
 int main()
 {
-	int Num1, Num2, Num3, Product;
+	int Number1, Number2, Number3, Product;
 
 	printf("To multiply three numbers.\n");
 	printf("Enter the first number: ");
-	scanf("%i", &Num1);
+	scanf("%i", &Number1);
 	printf("Enter the second number: ");
-	scanf("%i", &Num2);
+	scanf("%i", &Number2);
 	printf("Enter the third number: ");
-	scanf("%i", &Num3);
-	Product = Num1*Num2*Num3;
-	printf("the product of %i, %i and %i is %i.", Num1, Num2, Num3, Product);
+	scanf("%i", &Number3);
+	Product = Number1*Number2*Number3;
+	printf("the product of %i, %i and %i is %i.", Number1, Number2, Number3, Product);
 
 
 
