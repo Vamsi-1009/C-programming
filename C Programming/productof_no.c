49c94cfbaab405787edcc075990a2cff31cfbff6
vamsi <vamsikrishnapallam@gmail.com>
2025-09-11 14:37:37 +0530

    changed variables to small.

--- C Programming/productof_no.c
+++ C Programming/productof_no.c
@@ -16,17 +16,17 @@ step11: stop
 
 int main()
 {
-	int Number1, Number2, Number3, Product;
+	int number1, number2, number3, product;
 
 	printf("To multiply three numbers.\n");
 	printf("Enter the first number: ");
-	scanf("%i", &Number1);
+	scanf("%i", &number1);
 	printf("Enter the second number: ");
-	scanf("%i", &Number2);
+	scanf("%i", &number2);
 	printf("Enter the third number: ");
-	scanf("%i", &Number3);
-	Product = Number1*Number2*Number3;
-	printf("the product of %i, %i and %i is %i.", Number1, Number2, Number3, Product);
+	scanf("%i", &number3);
+	product = number1*number2*number3;
+	printf("the product of %i, %i and %i is %i.", number1, number2, number3, product);
 
 
 
