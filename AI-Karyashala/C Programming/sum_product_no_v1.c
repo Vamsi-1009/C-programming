4bcac12b3fca4a1e3ec730600c5a9d3bb6e2b6a9
Vamsi krishna <vamsikrishnapallam@gmail.com>
2025-09-13 00:49:00 +0530

    ! is added to the condition.

--- AI-Karyashala/C Programming/sum_product_no_v1.c
+++ AI-Karyashala/C Programming/sum_product_no_v1.c
@@ -1,17 +1,17 @@
-/*step 1: start
-step 2: print "To find two numbers from their sum and product .\n"
-step 3: print "Enter their sum: "
-step 4: read Sum
-step 5: print "Enter their product: "
-step 6: read Product
-step 7: Num1 = 1
-step 8: Num2 = Sum - Num1
-step 9: if (Num1 * Num2) not= Product
-           Num1 = Num1 + 1
-           Num2 = Sum - Num1
-           goto step9
-step 10: print "The two numbers are " + Num1 + " and " + Num2 + "."
-step 11:stop*/
+/*step-1: start
+step-2: print "To find two numbers from their sum and product .\n"
+step-3: print "Enter their sum: "
+step-4: read Sum
+step-5: print "Enter their product: "
+step-6: read Product
+step-7: Num1 = 1
+step-8: Num2 = Sum - Num1
+step-9: if (Num1 * Num2) != Product
+          Num1 = Num1 + 1
+          Num2 = Sum - Num1
+          goto step-9
+step-10: print "The two numbers are " + Num1 + " and " + Num2 + "."
+step-11:stop*/
 
 #include<stdio.h>
 
